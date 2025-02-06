ec259188f94f7d21474c8a7e5561d766b3e52dfd
rinkugupta3 <dhirajgupta@hotmail.com>
2025-02-06 13:11:21 -0500

    Modify iphone and macbook modules function

--- modules/macbook_actions.py
+++ modules/macbook_actions.py
@@ -12,10 +12,11 @@ def handle_macbook(page):
     Handle actions related to MacBook Pro.
     """
     logger.info("Handling MacBook Pro actions.")
-    page.locator("section").filter(has_text="MacBook Pro Mind-blowing.Head").get_by_label("Buy, MacBook Pro").click()
+
+    page.get_by_role("link", name="Buy, MacBook Pro").nth(0).click(timeout=60000)
     page.get_by_role("radio", name="16-inch").click()
-    page.get_by_role("tab", name="M3 Pro").click()
-    page.get_by_role("button", name="Select Apple M3 Pro with 12-").nth(1).click()
+    page.get_by_role("tab", name="M4 Pro").click()
+    page.get_by_role("button", name="Select Apple M4 Pro with 14-").nth(0).click()
     page.get_by_role("button", name="Add to Bag").click()
     page.wait_for_timeout(5000)
     page.screenshot(path='screenshots/MacBook_Pro.png')
