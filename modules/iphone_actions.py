72d7c048406b64be99f99b5bb013d373bfed8f7d
rinkugupta3 <dhirajgupta@hotmail.com>
2025-02-22 13:07:24 -0500

    modify iphone_actions.py file

--- modules/iphone_actions.py
+++ modules/iphone_actions.py
@@ -9,14 +9,15 @@ logger = logging.getLogger()
 
 
 def handle_iphone(page):
-    """
-    Handle actions related to iPhone 16 Pro Max.has_text="256GB Footnote ² From $1099"
-    """
+    """Handle actions related to iPhone 16 Pro."""
     logger.info("Handling iPhone 16 Pro Max actions.")
+    """Click iphone buy button"""
     page.wait_for_timeout(10000)
+    page.get_by_role('link', name='Buy iPhone 16 Pro').nth(2).click(timeout=60000)
+
     # page.locator("section").filter(has_text="iPhone 16 Pro Hello, Apple").get_by_label("Buy iPhone 16 Pro").click()
-    page.locator("section:has-text('iPhone 16 Pro Hello, Apple')").locator('a[aria-label="Buy iPhone 16 Pro"]').nth(
-        0).click(timeout=60000)
+    # page.locator("section:has-text('iPhone 16 Pro Hello, Apple')").locator('a[aria-label="Buy iPhone 16 Pro"]')\
+    #    .nth(0).click(timeout=60000)
 
     page.get_by_text('iPhone 16 Pro 6.3-inch').click()
     page.get_by_role('listitem').filter(has_text='Desert Titanium').locator('img').click()
